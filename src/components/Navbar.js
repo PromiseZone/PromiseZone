31922991b5b57ffabafada4c8f2b3153b9c4c488
Julie K <julieskim160@gmail.com>
2018-10-12 12:25:20 -0700

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 function Navbar() {
     return (
-        
+        <nav>
+            <div className="nav-wrapper">
+                <ul className="nav">
+                    <li><a href="#">Economic Opportunity</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
