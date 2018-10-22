f199b2aa7d29fe28c7933f41dc2a2ada222e8ade
Julie K <julieskim160@gmail.com>
2018-10-21 20:22:16 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>LA Promise Zone</p>
+        {/* <header className="App-header"> */}
+          <p className="App__population">LA Promise Zone</p>
+          <p className="App__partnets">Partnets 74+</p>
+          <p className="App__outcomes">Measurable Outcomes 13+</p>
           <Navbar />
-        </header>
+        {/* </header> */}
       </div>
     );
   }
