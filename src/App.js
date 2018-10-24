68043cd9eadbb93f8276fb8b6e6ca674fecf821e
Julie K <julieskim160@gmail.com>
2018-10-24 12:53:44 -0700

    set up static top informaiton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header"> */}
+        <header className="App-header">
           <p className="App__population">Population 170,000</p>
           <p className="App__partnets">Partnets 74+</p>
           <p className="App__outcomes">Measurable Outcomes 13+</p>
+          
+        </header>
           <Navbar />
-        {/* </header> */}
       </div>
     );
   }
